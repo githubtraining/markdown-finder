38ed42b57bf00065f651ba65e1707fd19549f2f2
Jason Etcovitch <jasonetco@gmail.com>
2018-02-09 19:40:41 -0700

    Cleanup, add multiples

--- test/index.test.js
+++ test/index.test.js
@@ -20,7 +20,7 @@ describe('MarkdownFinder', () => {
     })
 
     describe('link', () => {
-      it('returns true if the markdown contains an link', () => {
+      it('returns true if the markdown contains a link', () => {
         const md = 'This is a link [link](example.com)'
         expect(finder(md).has.link()).toBe(true)
       })
@@ -33,6 +33,30 @@ describe('MarkdownFinder', () => {
       it('does not match image links', () => {
         const md = 'This is an image ![img](url)'
         expect(finder(md).has.link()).toBe(false)
+      })
+    })
+
+    describe('images', () => {
+      it('returns true if there are multiple images', () => {
+        const md = 'This is an image ![img](example.com/image.png) and so is this ![img](example.com/photo.png)'
+        expect(finder(md).has.images()).toBe(true)
+      })
+
+      it('returns false if it does not exist', () => {
+        const md = 'No images here'
+        expect(finder(md).has.images()).toBe(false)
+      })
+    })
+
+    describe('links', () => {
+      it('returns an array of links', () => {
+        const md = 'This is a link [link](example.com) and so is [this](this.com)'
+        expect(finder(md).has.links()).toBe(true)
+      })
+
+      it('returns an empty array if the markdown does not contain any links', () => {
+        const md = 'This is not an link'
+        expect(finder(md).has.links()).toBe(false)
       })
     })
   })
@@ -51,7 +75,7 @@ describe('MarkdownFinder', () => {
     })
 
     describe('link', () => {
-      it('returns true if the markdown contains an link', () => {
+      it('returns a link', () => {
         const md = 'This is a link [link](example.com)'
         expect(finder(md).get.link()).toMatchSnapshot()
       })
@@ -59,6 +83,37 @@ describe('MarkdownFinder', () => {
       it('returns false if the markdown does not contain a link', () => {
         const md = 'This is not an link'
         expect(finder(md).get.link()).toBe(false)
+      })
+    })
+
+    describe('images', () => {
+      it('returns an array of images', () => {
+        const md = 'This is an image ![img](example.com/image.png) and so is this ![img](example.com/photo.png)'
+        const images = finder(md).get.images()
+        expect(Array.isArray(images)).toBe(true)
+        expect(images).toMatchSnapshot()
+      })
+
+      it('returns false if it does not exist', () => {
+        const md = 'No images here'
+        expect(finder(md).get.images()).toEqual([])
+      })
+    })
+
+    describe('links', () => {
+      it('returns an array of links', () => {
+        const md = 'This is a link [link](example.com) and so is [this](this.com)'
+        expect(finder(md).get.links()).toMatchSnapshot()
+      })
+
+      it('returns an empty array if the markdown does not contain any links', () => {
+        const md = 'This is not an link'
+        expect(finder(md).get.links()).toEqual([])
+      })
+
+      it('returns an array of links but not images', () => {
+        const md = 'This is a link [link](example.com) and this is an image ![this](this.com)'
+        expect(finder(md).get.links().length).toBe(1)
       })
     })
   })
